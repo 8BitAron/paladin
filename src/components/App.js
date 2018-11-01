73a73c07abaad365e8bec5493b2c3975f4b4fe45
Aron Braggans <aronbraggans@cargillmacbookpro.local>
2018-11-01 16:00:59 -0500

    Break Point

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react'
 
 class App extends Component {
-  render() {
-    return (
-      <section className="hero is-info is-fullheight">
-        <div className="hero-body">
-          <div className="container">
-            <h1 className="title">
-              <i class="fas fa-cat" /> Boiler Plate
-            </h1>
-            <h2 className="subtitle">Create React App, Bulma CSS, Font Awesome, Webpack, Redux, React Router</h2>
-            <p>Extra Credit Serverless and Markdown</p>
-          </div>
-        </div>
-      </section>
-    )
-  }
+    render() {
+        return (
+            <section className="hero is-info is-fullheight">
+                <div className="hero-body">
+                    <div className="container">
+                        <h1 className="title">
+                            <i className="fas fa-cat" /> Boiler Plate
+                        </h1>
+                        <h2 className="subtitle">
+                            Create React App, Bulma CSS, Font Awesome, Webpack, Redux, React Router
+                        </h2>
+                        <p>Extra Credit Serverless and Markdown</p>
+                    </div>
+                </div>
+            </section>
+        )
+    }
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -2,18 +2,20 @@ import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { ConnectedRouter } from 'connected-react-router'
-import store, { history } from './store'
-
-import ReactDOM from 'react-dom'
-
+import { store, history } from './store'
+import App from './components/App'
+import * as serviceWorker from './serviceWorker'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import './index.scss'
 
-import App from './components/App'
-import * as serviceWorker from './serviceWorker'
-
-ReactDOM.render(<App />, document.getElementById('root'))
-
+render(
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            <App />
+        </ConnectedRouter>
+    </Provider>,
+    document.getElementById('root')
+)
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
