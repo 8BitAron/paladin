ec29c2d1228167e57254967a2a7ee88e2d372708
Aron Braggans <aronbraggans@Eleanor-Rigby.lan>
2018-11-12 19:27:50 -0600

    adding thunk action creator

--- src/actions/counter.js
+++ src/actions/counter.js
@@ -2,13 +2,23 @@ export const UP = 'counter/UP'
 export const DOWN = 'counter/DOWN'
 
 export function up() {
-    return {
-        type: UP
-    }
+  return {
+    type: UP
+  }
 }
 
 export function down() {
-    return {
-        type: DOWN
+  return function(dispatch, getState) {
+    if (getState().counter.count === 0) {
+      return Promise.resolve()
     }
+
+    dispatch(subtract())
+  }
+}
+
+function subtract() {
+  return {
+    type: DOWN
+  }
 }
